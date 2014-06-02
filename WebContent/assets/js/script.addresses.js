c1f8b23d23b69f4a997186792181f96ae18444a7
Michaël Zasso <mic.besace@gmail.com>
2014-06-02 16:14:39 +0200

    restore last desired state

--- WebContent/assets/js/script.addresses.js
+++ WebContent/assets/js/script.addresses.js
@@ -15,5 +15,17 @@
 			jsonRender : base + "script/renders/jsonreport/index.html?url=",
 			jsonColorRender : base + "script/renders/jsonColor.html?url=",
 			visualizerReleases: base + "visualizer/release/list.shtml",
+			getVisualizerParts: function(){
+				var head = $visualizerRelease==="HEAD",
+					visualizerVersionURL = "visualizer/release/"+ $visualizerRelease +(head ? "/src" : "")+"/index.html" ;
+				var startLink = base + visualizerVersionURL;
+				if(!head) head = ($visualizerRelease==='current'||$visualizerRelease.indexOf("2014")===0);
+				var suffix = ((!head) ? ("&header=" + encodeURIComponent(base) + "headers/default.json"+"&config=" + encodeURIComponent(base) + "configs/default.json") : "&config="+encodeURIComponent(base+"configs/head.json"));
+				return {
+					prefix: startLink,
+					suffix: suffix
+				};
+			}
 		};
+		
 	};
