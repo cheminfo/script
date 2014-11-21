57c499f70ff4bf4936c4f75537f7822dea4c286e
Luc Patiny <luc@patiny.com>
2014-11-21 11:28:46 +0100

    Allows to deal with new lactame release

--- WebContent/assets/js/script.addresses.js
+++ WebContent/assets/js/script.addresses.js
@@ -19,7 +19,7 @@
 				var head = $visualizerRelease==="HEAD",
 					visualizerVersionURL = "visualizer/release/"+ $visualizerRelease +(head ? "/src" : "")+"/index.html" ;
 				var startLink = base + visualizerVersionURL;
-				if(!head) head = ($visualizerRelease==='current'||$visualizerRelease.indexOf("2014")===0);
+				if(!head) head = ($visualizerRelease==='current'||$visualizerRelease.indexOf("2014")===0||$visualizerRelease.indexOf("v")===0);
 				var suffix = ((!head) ? ("&header=" + encodeURIComponent(base) + "headers/default.json"+"&config=" + encodeURIComponent(base) + "configs/default.json") : "&config="+encodeURIComponent(base+"configs/head.json"));
 				return {
 					prefix: startLink,
