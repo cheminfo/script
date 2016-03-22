0bd6430ac3178dbe09a0dcb0947072dfe31e347d
Luc Patiny <luc@patiny.com>
2016-03-22 21:10:27 +0100

    Update adress for https

--- WebContent/assets/js/script.addresses.js
+++ WebContent/assets/js/script.addresses.js
@@ -14,7 +14,7 @@
 			run: base + "script/Run",
 			jsonRender : base + "script/renders/jsonreport/index.html?url=",
 			jsonColorRender : base + "script/renders/jsonColor.html?url=",
-			visualizerReleases: "http://www.lactame.com/visualizer/list.php",
+			visualizerReleases: "https://www.lactame.com/visualizer/list.php",
 			getVisualizerParts: function(){ 
 			//	var head = $visualizerRelease==="HEAD";
 			//	var suffix = ((!head) ? ("&header=" + encodeURIComponent(base) + "headers/default.json"+"&config=" + encodeURIComponent(base) + "configs/default.json") : "&config="+encodeURIComponent(base+"configs/head.json"));
